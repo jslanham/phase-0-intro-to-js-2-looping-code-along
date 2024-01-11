dfee7ee48fb0834b9a7f00b1c38d050982dd97d2
Joseph Lanham <jslanham2020@gmail.com>
2024-01-11 12:37:01 -0500

    Loops complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+const cards = []
+function wrapGifts(gift) {
+    for (let i = 0; i < gifts.length; i++){
+    console.log(`Wrapped ${gift} and added a bow!`);
+    }
+    return gifts;
+}
+function writeCards(people, occasion){
+    const writtenCards = [];
+    for (let i = 0; i < people.length; i++){
+        writtenCards.push(`Thank you, ${people[i]}, for the wonderful ${occasion} gift!`);
+        debugger;
+    }
+    return writtenCards;
+}
+function countDown(x){
+    while(x >= 0){
+        console.log(x--);
+    }
+}
